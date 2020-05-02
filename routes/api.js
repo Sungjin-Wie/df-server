3f38c60d08943599047bac9ef488df6843ccbcce
Sungjin-Wie <tmdwls1203@gmail.com>
2020-05-03 03:12:34 +0900

    add request

--- routes/api.js
+++ routes/api.js
@@ -2,13 +2,18 @@ var express = require('express');
 var router = express.Router();
 const apiKey = require('./data').apiKey;
 var _url = require('./data').url;
+var request = require('request');
 
 router.get('/search', function (req, res, next) {
   let url = _url;
   let name = encodeURIComponent(req.query.name);
   console.log(name);
   url += `df/servers/${req.query.server}/characters?characterName=${name}&wordType=full&limit=200&apikey=${apiKey}`;
-  res.redirect(url);
-});
+  request(url, function (error, response, body) {
+    if (!error) {
+      res.send(body);
+    }
+  }); // request
+}); // router.get
 
 module.exports = router;
