5ede0d4c0351f1ec2ae06645515508b2d0124d16
Sungjin-Wie <tmdwls1203@gmail.com>
2020-05-05 22:33:26 +0900

    [Add] /info

--- routes/api.js
+++ routes/api.js
@@ -31,18 +31,34 @@ router.get('/auction', function (req, res, next) {
 router.get('/info', function (req, res, next) {
   let serverId = encodeURIComponent(req.query.server);
   let characterId = encodeURIComponent(req.query.id);
-  let resbody = {};
   let stat =
     _url +
     `df/servers/${serverId}/characters/${characterId}/status?apikey=${apiKey}`;
-  console.log(stat);
-  request(stat, function (error, response1, body1) {
-    if (!error) {
-      resbody.status = body1;
-      console.log(resbody);
-      res.send(resbody);
-    }
-  });
+  let equip =
+    _url +
+    `df/servers/${serverId}/characters/${characterId}/equip/equipment?apikey=${apiKey}`;
+  let avatar =
+    _url +
+    `df/servers/${serverId}/characters/${characterId}/equip/avatar?apikey=${apiKey}`;
+  let creature =
+    _url +
+    `df/servers/${serverId}/characters/${characterId}/equip/creature?apikey=${apiKey}`;
+  let flag =
+    _url +
+    `df/servers/${serverId}/characters/${characterId}/equip/flag?apikey=${apiKey}`;
+  let urls = [stat, equip, avatar, creature, flag];
+  Promise.all(urls.map(requestAsync))
+    .then((res) => Object.assign({}, res[0], res[1], res[2], res[3], res[4]))
+    .then((data) => res.send(data));
 });
+
+var requestAsync = function (url) {
+  return new Promise((resolve, reject) => {
+    var req = request(url, (err, response, body) => {
+      if (err) return reject(err, response, body);
+      resolve(JSON.parse(body));
+    });
+  });
+};
 
 module.exports = router;
